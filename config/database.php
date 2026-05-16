1a6f9f9a1847d10f82f3e9dcd9479002add81ca7
bimeri noel <bimerinoel@gmail.com>
2026-05-17 00:13:06 +0300

    correction of some text

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                'mysql_attr_ssl_ca' => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
